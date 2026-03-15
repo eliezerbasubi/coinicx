6be316cc9c3f80f3368302f169b37b0981ecaf6f
eliezerbasubi <eliezer.basubi30@gmail.com>
2026-03-15 14:00:28 +0300

    feat: Integrate Serwist for PWA capabilities, enhance security headers, and refine Next.js build configuration.

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,20 @@
 import type { NextConfig } from "next";
+import withSerwistInit from "@serwist/next";
+
+const withSerwist = withSerwistInit({
+  swSrc: "src/app/sw.ts",
+  swDest: "public/sw.js",
+  disable: process.env.NODE_ENV !== "production",
+});
 
 const nextConfig: NextConfig = {
+  compiler: {
+    removeConsole:
+      process.env.NODE_ENV === "production" &&
+      process.env.NEXT_PUBLIC_WEB3_NETWORK === "mainnet",
+  },
   images: {
+    minimumCacheTTL: 31536000, // 1 year
     remotePatterns: [
       {
         protocol: "https",
@@ -10,6 +23,50 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  serverExternalPackages: ["pino", "lokijs", "encoding"],
+  async headers() {
+    return [
+      {
+        source: "/(.*)?", // Matches all pages
+        headers: [
+          {
+            key: "X-Frame-Options",
+            value: "DENY",
+          },
+          {
+            key: "X-Content-Type-Options",
+            value: "nosniff",
+          },
+          {
+            key: "Strict-Transport-Security",
+            value: "max-age=630720; includeSubDomains; preload",
+          },
+          {
+            key: "Cross-Origin-Opener-Policy",
+            value: "same-origin",
+          },
+          {
+            key: "Referrer-Policy",
+            value: "strict-origin",
+          },
+          {
+            key: "X-Xss-Protection",
+            value: "1; mode=block",
+          },
+          {
+            key: "X-Permitted-Cross-Domain-Policies",
+            value: "none",
+          },
+          { key: "X-DNS-Prefetch-Control", value: "on" },
+          {
+            key: "Permissions-Policy",
+            value:
+              "accelerometer=(),autoplay=(),camera=(),clipboard-read=(),clipboard-write=(self),geolocation=(),gyroscope=(),hid=(),magnetometer=(),microphone=(),payment=(),publickey-credentials-get=(),screen-wake-lock=(),serial=(),usb=()",
+          },
+        ],
+      },
+    ];
+  },
 };
 
-export default nextConfig;
+export default withSerwist(nextConfig);
