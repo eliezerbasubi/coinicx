62750900932a4cd19f23fd215f37e7c60f9952cd
eliezerbasubi <eliezer.basubi30@gmail.com>
2026-03-17 03:36:00 +0300

    refactor: updated splash screen

--- src/features/trade/loading.tsx
+++ src/features/trade/loading.tsx
@@ -11,12 +11,12 @@ const TradingLoading = () => {
             <div className="hidden md:block w-full lg:max-w-65 xl:max-w-80 md:h-67 lg:h-auto bg-primary-dark rounded-md" />
 
             {/* TradeChartArea */}
-            <div className="w-full xl:flex-1 bg-primary-dark rounded-md h-96 lg:h-125" />
+            <div className="w-full xl:flex-1 bg-primary-dark md:rounded-md h-96 lg:h-125" />
           </div>
         </div>
 
         {/* OrderForm */}
-        <div className="w-full max-w-80 lg:max-w-65 xl:max-w-80 flex flex-col gap-y-1">
+        <div className="w-full max-w-80 lg:max-w-65 xl:max-w-80 hidden md:flex flex-col gap-y-1">
           {/* A fake div to show that the ticker is full width - only visible in table */}
           {/* Hide the edges by adding and removing one pixel */}
           <div className="w-[calc(100%+1px)] -ml-px h-20 hidden md:block xl:hidden bg-primary-dark rounded-r-md" />
@@ -24,14 +24,14 @@ const TradingLoading = () => {
           {/* Real OrderForm */}
           {/* Add 1px padding left to push the orderbook form  */}
           <div className="w-full flex-1 md:pl-1 xl:pl-0">
-            <div className="size-full bg-primary-dark rounded-md" />
+            <div className="size-full bg-primary-dark md:rounded-md" />
           </div>
         </div>
       </div>
 
       <div className="w-full bg-trade-dark flex gap-1 py-0.5 md:px-1 md:pb-2 md:pt-0 flex-wrap md:flex-nowrap">
         {/* TradeUserInfo */}
-        <div className="w-full h-85 bg-primary-dark rounded-md" />
+        <div className="w-full h-85 bg-primary-dark md:rounded-md" />
 
         {/* UserAccountInfo */}
         <div className="w-full max-w-80 md:max-w-79 lg:max-w-64.5 xl:max-w-80 bg-primary-dark rounded-md" />
